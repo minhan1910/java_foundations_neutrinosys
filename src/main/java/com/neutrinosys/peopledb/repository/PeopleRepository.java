6fcbd6e8f64be4b2c07d7fa5c00254fafea5502c
An <annogo123@gmail.com>
2022-06-13 20:37:37 +0700

    initialize project

---
+++ src/main/java/com/neutrinosys/peopledb/repository/PeopleRepository.java
@@ -0,0 +1,160 @@
+package com.neutrinosys.peopledb.repository;
+
+import com.neutrinosys.peopledb.model.Person;
+
+import java.math.BigDecimal;
+import java.sql.*;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.stream.Collectors.joining;
+
+public class PeopleRepository {
+    public static final String FIND_BY_ID_SQL = "SELECT ID, FIRST_NAME, LAST_NAME, DOB, SALARY FROM PEOPLE WHERE ID = ?";
+    public static final String SAVE_PERSON_SQL = "INSERT INTO PEOPLE (FIRST_NAME, LAST_NAME, DOB) VALUES (?, ?, ?)";
+    public static final String FIND_ALL_SQL = "SELECT ID, FIRST_NAME, LAST_NAME, DOB, SALARY FROM PEOPLE";
+    private Connection connection;
+
+    public PeopleRepository(Connection connection) {
+        this.connection = connection;
+    }
+
+    // Ctrl + Alt + C -> extract string into const of  member class
+    public Person save(Person person) {
+        String sql = String.format(SAVE_PERSON_SQL);
+        try {
+            PreparedStatement ps = this.connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, person.getFirstName());
+            ps.setString(2, person.getLastName());
+            ps.setTimestamp(3, convertDobToTimestamp(person.getDob()));
+            int recordAffected = ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            while (rs.next()) {
+                long id = rs.getLong(1);
+                person.setId(id);
+            }
+            System.out.printf("Records affected: %d\n", recordAffected);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return person;
+    }
+
+    public Optional<Person> findById(Long id) {
+        Person person = null;
+        try {
+            PreparedStatement ps = this.connection.prepareStatement(FIND_BY_ID_SQL);
+            ps.setLong(1, id);
+            ResultSet rs = ps.executeQuery();// for query
+            while (rs.next()) {
+                person = extractPersonFromResultSet(rs);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return Optional.ofNullable(person);
+    }
+
+    private Person extractPersonFromResultSet(ResultSet rs) throws SQLException {
+        long personId = rs.getLong("ID");
+        String firstName = rs.getString("FIRST_NAME");
+        String lastName = rs.getString("LAST_NAME");
+        ZonedDateTime dob = ZonedDateTime.of(rs.getTimestamp("DOB").toLocalDateTime(), ZoneId.of("+0"));// +0 -> work every time zone
+        BigDecimal salary = rs.getBigDecimal("SALARY");
+        return new Person(personId, firstName, lastName, dob, salary);
+    }
+
+    public long count() {
+        long result = 0L;
+        try {
+            PreparedStatement ps = this.connection.prepareStatement("SELECT COUNT(*) FROM PEOPLE");
+            ResultSet rs = ps.executeQuery();
+            while(rs.next())
+                result = rs.getLong(1);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
+
+    public List<Person> findAll() {
+        List<Person> result = new ArrayList<Person>();
+        try {
+            PreparedStatement ps = this.connection.prepareStatement(FIND_ALL_SQL);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                Long id = rs.getLong("ID");
+                String firstName = rs.getString("FIRST_NAME");
+                String lastName = rs.getString("LAST_NAME");
+                ZonedDateTime dob = ZonedDateTime.ofInstant(rs.getTimestamp("DOB").toInstant(), ZoneId.of("+0"));
+                Person newPerson = new Person(firstName, lastName, dob);
+                newPerson.setId(id);
+                result.add(newPerson);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
+
+    public void delete(Person person) {
+        try {
+            PreparedStatement ps = this.connection.prepareStatement("DELETE FROM PEOPLE WHERE ID = ?");
+            ps.setLong(1, person.getId());
+            int affectedRecordCount = ps.executeUpdate();
+            System.out.println(affectedRecordCount);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public void delete(Person... people) {
+        // Handling with imperative programming
+//        StringBuilder ids = new StringBuilder(people.length);
+//        String prefix = "";
+//        for (Person person : people) {
+//            ids.append(prefix);
+//            prefix = ",";
+//            ids.append(person.getId().toString());
+//        }
+
+        // with funional programming
+        String ids = Arrays.stream(people)
+                .map(Person::getId)
+                .map(String::valueOf)
+                .collect(joining(","));
+        try {
+            Statement stmt = this.connection.createStatement();
+            int affectedRecordCount = stmt.executeUpdate("DELETE FROM PEOPLE WHERE ID IN(:ids)".replace(":ids", ids));
+            System.out.println(affectedRecordCount);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void update(Person person) {
+        try {
+            PreparedStatement ps = this.connection.prepareStatement("UPDATE PEOPLE SET FIRST_NAME=?, LAST_NAME=?, DOB=?, SALARY=? WHERE ID=?");
+            ps.setString(1, person.getFirstName());
+            ps.setString(2, person.getLastName());
+            ps.setTimestamp(3, this.convertDobToTimestamp(person.getDob()));
+            ps.setBigDecimal(4, person.getSalary());
+            ps.setLong(5, person.getId());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // Ctrl + Alt + P no se doi tu person.dob -> ZonedDateTime dob
+    private Timestamp convertDobToTimestamp(ZonedDateTime dob) {
+        return Timestamp.valueOf(dob.withZoneSameInstant(ZoneId.of("+0")).toLocalDateTime());
+    }
+
+}
---
+++ src/test/java/com/neutrinosys/peopledb/repository/PeopleRepositoryTests.java
@@ -0,0 +1,146 @@
+package com.neutrinosys.peopledb.repository;
+
+import com.neutrinosys.peopledb.model.Person;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.joining;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PeopleRepositoryTests {
+    private PeopleRepository repo;
+    private Connection connection;
+
+
+    @BeforeEach
+    void setUp() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //.replace("~", System.getProperty("user.home"))
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/peopletest", "root", "");
+            this.repo = new PeopleRepository(connection);
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void canSaveOnePerson() {
+        Person john = new Person("An", "Smith", ZonedDateTime.of(2002, 11, 19, 15, 15, 0, 0, ZoneId.of("-6")));
+        Person savedPerson = repo.save(john);
+        assertThat(savedPerson.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void canSaveTwoPeople() {
+        Person john = new Person("John", "Smith", ZonedDateTime.of(1980, 11, 15, 15, 15, 0, 0, ZoneId.of("-6")));
+        Person bobby = new Person("Bobby", "Smith", ZonedDateTime.of(1982, 9, 13, 13, 13, 0, 0, ZoneId.of("-8")));
+        Person savedPerson1 = repo.save(john);
+        Person savedPerson2 = repo.save(bobby);
+        assertThat(savedPerson1.getId()).isNotEqualTo(savedPerson2.getId());
+    }
+
+    /**
+     * This test not match by DOB field
+     */
+    @Test
+    public void canFindPersonById() {
+        Person savedPerson = this.repo.save(new Person("test", "jackson", ZonedDateTime.now()));
+        Person foundPerson = this.repo.findById(savedPerson.getId()).get(); // .get by Optional
+        assertThat(foundPerson).isEqualTo(savedPerson);
+    }
+
+    @Test
+    public void testPersonIdNotFound() {
+        Optional<Person> foundPerson = this.repo.findById(-1L);
+        assertThat(foundPerson).isEmpty();
+    }
+
+    @Test
+    public void canGetAllPeople() {
+//        this.repo.save(new Person("test", "jackson", ZonedDateTime.now()));
+//        this.repo.save(new Person("test", "jackson", ZonedDateTime.now()));
+//        this.repo.save(new Person("test", "jackson", ZonedDateTime.now()));
+//        this.repo.save(new Person("test", "jackson", ZonedDateTime.now()));
+//        this.repo.save(new Person("test", "jackson", ZonedDateTime.now()));
+//        this.repo.save(new Person("test", "jackson", ZonedDateTime.now()));
+//        this.repo.save(new Person("test", "jackson", ZonedDateTime.now()));
+//        this.repo.save(new Person("test", "jackson", ZonedDateTime.now()));
+//        this.repo.save(new Person("test", "jackson", ZonedDateTime.now()));
+//        this.repo.save(new Person("test", "jackson", ZonedDateTime.now()));
+        List<Person> people = this.repo.findAll();
+        assertThat(people).hasSizeGreaterThan(10);
+    }
+
+    @Test
+    public void canGetCount() {
+        long startCount = this.repo.count();
+        this.repo.save(new Person("test", "jackson", ZonedDateTime.now()));
+        this.repo.save(new Person("test", "jackson", ZonedDateTime.now()));
+        long endCount = this.repo.count();
+        assertThat(endCount).isEqualTo(startCount + 2);
+    }
+
+    @Test
+    public void canDelete() {
+        Person savedPerson = this.repo.save(new Person("test", "jackson", ZonedDateTime.now()));
+        long startCount = this.repo.count();
+        this.repo.delete(savedPerson);
+        long endCount = this.repo.count();
+        assertThat(endCount).isEqualTo(startCount - 1);
+    }
+
+    @Test
+    public void canDeleteMultiplePeople() {
+        Person p1 = this.repo.save(new Person("test", "jackson", ZonedDateTime.now()));
+        Person p2 = this.repo.save(new Person("test", "jackson", ZonedDateTime.now()));
+
+        long startCount = this.repo.count();
+        this.repo.delete(p1, p2);
+        long endCount = this.repo.count();
+        assertThat(endCount).isEqualTo(startCount - 2);
+    }
+
+    @Test
+    public void canUpdate() {
+        Person savedPerson = this.repo.save(new Person("test", "jackson", ZonedDateTime.now()));
+
+        // capture the old savedPerson which is not updated the salary field
+        Person foundP = this.repo.findById(savedPerson.getId()).get();// get will return defined person
+
+        savedPerson.setSalary(new BigDecimal(73000.34));
+        this.repo.update(savedPerson);
+
+        Person foundP2 = this.repo.findById(savedPerson.getId()).get();
+
+        assertThat(foundP2.getSalary()).isNotEqualTo(foundP.getSalary());
+    }
+
+    @Test
+    public void experiment() {
+        Person p1 = new Person(28L, null, null, null);
+        Person p2 = new Person(35L, null, null, null);
+        Person p3 = new Person(29L, null, null, null);
+        Person p4 = new Person(40L, null, null, null);
+        Person p5 = new Person(31L, null, null, null);
+
+        // DELETE FROM PERSON WHERE ID (10, 20, 30, 40, 50);
+
+        // toArray take argument with type Person not Object
+        Person[] people = Arrays.asList(p1, p2, p3, p4, p5).toArray(new Person[]{});
+//        this.repo.delete(people);
+    }
+}
+
