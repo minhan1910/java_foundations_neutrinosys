e86478c25efe2e62afd9a888017ea099d24253f6
An <annogo123@gmail.com>
2022-06-14 07:41:43 +0700

    refactoring completely all repository methods

--- src/main/java/com/neutrinosys/peopledb/repository/CRUDRepository.java
+++ src/main/java/com/neutrinosys/peopledb/repository/CRUDRepository.java
@@ -1,10 +1,14 @@
 package com.neutrinosys.peopledb.repository;
 
 import com.neutrinosys.peopledb.model.Entity;
-import com.neutrinosys.peopledb.model.Person;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+
+import static java.util.stream.Collectors.joining;
 
 public abstract class CRUDRepository<T extends Entity> {
     protected Connection connection;
@@ -47,8 +51,82 @@ public abstract class CRUDRepository<T extends Entity> {
         return Optional.ofNullable(entity);
     }
 
-    abstract T extractEntityFromResultSet(ResultSet rs) throws SQLException;
+    public List<T> findAll() {
+        List<T> entities = new ArrayList<T>();
+        try {
+            PreparedStatement ps = this.connection.prepareStatement(getFindAllSql());
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                entities.add(extractEntityFromResultSet(rs));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return entities;
+    }
 
+    public long count() {
+        long result = 0L;
+        try {
+            PreparedStatement ps = this.connection.prepareStatement(getCountSql());
+            ResultSet rs = ps.executeQuery();
+            if (rs.next())
+                result = rs.getLong(1);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
+
+    public void delete(T entity) {
+        try {
+            PreparedStatement ps = this.connection.prepareStatement(getDeleteSql());
+            ps.setLong(1, entity.getId());
+            int affectedRecordCount = ps.executeUpdate();
+            System.out.println(affectedRecordCount);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void delete(T... entities) {
+        String ids = Arrays.stream(entities)
+                .map(T::getId)
+                .map(String::valueOf)
+                .collect(joining(","));
+        try {
+            Statement stmt = this.connection.createStatement();
+            int affectedRecordCount = stmt.executeUpdate(getDeleteInSql().replace(":ids", ids));
+            System.out.println(affectedRecordCount);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void update(T entity) {
+        try {
+            PreparedStatement ps = this.connection.prepareStatement(getUpdateSql());
+            mapForUpdate(entity, ps);
+            ps.setLong(5, entity.getId());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    protected abstract String getUpdateSql();
+
+    /**
+     *
+     * @return Should return a SQL string like:
+     * "DELETE FROM PEOPLE WHERE ID IN(:ids)"
+     * Be sure to include the '(:ids)' named parameter & call it 'ids'
+     */
+    protected abstract String getDeleteInSql();
+    protected abstract String getDeleteSql();
+    protected abstract String getCountSql();
+    protected abstract String getFindAllSql();
+    abstract T extractEntityFromResultSet(ResultSet rs) throws SQLException;
     /**
      *
      * @return a String that represents the SQL needed to retrieve one entity
@@ -56,9 +134,8 @@ public abstract class CRUDRepository<T extends Entity> {
      * entity's ID.
      */
     protected abstract String getFindByIdSql();
-
     abstract void mapForSave(T entity, PreparedStatement ps) throws SQLException;
-
+    abstract void mapForUpdate(T entity, PreparedStatement ps) throws SQLException;
     abstract String getSaveSql();
 }
 
--- src/main/java/com/neutrinosys/peopledb/repository/PeopleRepository.java
+++ src/main/java/com/neutrinosys/peopledb/repository/PeopleRepository.java
@@ -6,26 +6,20 @@ import java.math.BigDecimal;
 import java.sql.*;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 
-import static java.util.stream.Collectors.joining;
-
+// Ctrl + Alt + 0 : remove left over import
 public class PeopleRepository extends CRUDRepository<Person> {
-    public static final String FIND_BY_ID_SQL = "SELECT ID, FIRST_NAME, LAST_NAME, DOB, SALARY FROM PEOPLE WHERE ID = ?";
-    public static final String SAVE_PERSON_SQL = "INSERT INTO PEOPLE (FIRST_NAME, LAST_NAME, DOB) VALUES (?, ?, ?)";
-    public static final String FIND_ALL_SQL = "SELECT ID, FIRST_NAME, LAST_NAME, DOB, SALARY FROM PEOPLE";
+    private static final String FIND_BY_ID_SQL = "SELECT ID, FIRST_NAME, LAST_NAME, DOB, SALARY FROM PEOPLE WHERE ID = ?";
+    private static final String SAVE_PERSON_SQL = "INSERT INTO PEOPLE (FIRST_NAME, LAST_NAME, DOB) VALUES (?, ?, ?)";
+    private static final String SELECT_COUNT_SQL = "SELECT COUNT(*) FROM PEOPLE";
+    private static final String FIND_ALL_SQL = "SELECT ID, FIRST_NAME, LAST_NAME, DOB, SALARY FROM PEOPLE";
+    private static final String DELETE_SQL = "DELETE FROM PEOPLE WHERE ID = ?";
+    public static final String DELETE_IN_SQL = "DELETE FROM PEOPLE WHERE ID IN(:ids)";
+    public static final String UPDATE_SQL = "UPDATE PEOPLE SET FIRST_NAME=?, LAST_NAME=?, DOB=?, SALARY=? WHERE ID=?";
     private Connection connection;
 
     public PeopleRepository(Connection connection) {
         super(connection);
-    }
-
-    @Override
-    String getSaveSql() {
-        return PeopleRepository.SAVE_PERSON_SQL;
     }
 
     @Override
@@ -46,125 +40,46 @@ public class PeopleRepository extends CRUDRepository<Person> {
     }
 
     @Override
+    void mapForUpdate(Person entity, PreparedStatement ps) throws SQLException {
+        ps.setString(1, entity.getFirstName());
+        ps.setString(2, entity.getLastName());
+        ps.setTimestamp(3, this.convertDobToTimestamp(entity.getDob()));
+        ps.setBigDecimal(4, entity.getSalary());
+    }
+
+
+    @Override
+    String getSaveSql() {
+        return PeopleRepository.SAVE_PERSON_SQL;
+    }
+
+    @Override
     protected String getFindByIdSql() {
         return FIND_BY_ID_SQL;
     }
 
-    //    // Ctrl + Alt + C -> extract string into const of  member class
-//    public Person save(Person person) {
-//        String sql = String.format(SAVE_PERSON_SQL);
-//        try {
-//            PreparedStatement ps = this.connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-////            mapForSave(ps, person, convertDobToTimestamp(person.getDob()));
-//            int recordAffected = ps.executeUpdate();
-//            ResultSet rs = ps.getGeneratedKeys();
-//            while (rs.next()) {
-//                long id = rs.getLong(1);
-//                person.setId(id);
-//            }
-//            System.out.printf("Records affected: %d\n", recordAffected);
-//        } catch (SQLException e) {
-//            System.out.println(e.getMessage());
-//        }
-//        return person;
-//    }
-
-    public Optional<Person> findById(Long id) {
-        Person person = null;
-        try {
-            PreparedStatement ps = super.connection.prepareStatement(FIND_BY_ID_SQL);
-            ps.setLong(1, id);
-            ResultSet rs = ps.executeQuery();// for query
-            while (rs.next()) {
-                person = extractEntityFromResultSet(rs);
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return Optional.ofNullable(person);
+    @Override
+    protected String getFindAllSql() {
+        return FIND_ALL_SQL;
     }
 
-    public long count() {
-        long result = 0L;
-        try {
-            PreparedStatement ps = super.connection.prepareStatement("SELECT COUNT(*) FROM PEOPLE");
-            ResultSet rs = ps.executeQuery();
-            while(rs.next())
-                result = rs.getLong(1);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return result;
+    @Override
+    protected String getCountSql() {
+        return SELECT_COUNT_SQL;
     }
 
-    public List<Person> findAll() {
-        List<Person> result = new ArrayList<Person>();
-        try {
-            PreparedStatement ps = super.connection.prepareStatement(FIND_ALL_SQL);
-            ResultSet rs = ps.executeQuery();
-            while(rs.next()) {
-                Long id = rs.getLong("ID");
-                String firstName = rs.getString("FIRST_NAME");
-                String lastName = rs.getString("LAST_NAME");
-                ZonedDateTime dob = ZonedDateTime.ofInstant(rs.getTimestamp("DOB").toInstant(), ZoneId.of("+0"));
-                Person newPerson = new Person(firstName, lastName, dob);
-                newPerson.setId(id);
-                result.add(newPerson);
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return result;
+    @Override
+    protected String getDeleteSql() {
+        return DELETE_SQL;
     }
 
-    public void delete(Person person) {
-        try {
-            PreparedStatement ps = super.connection.prepareStatement("DELETE FROM PEOPLE WHERE ID = ?");
-            ps.setLong(1, person.getId());
-            int affectedRecordCount = ps.executeUpdate();
-            System.out.println(affectedRecordCount);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-
+    @Override
+    protected String getDeleteInSql() {
+        return DELETE_IN_SQL;
     }
-
-    public void delete(Person... people) {
-        // Handling with imperative programming
-//        StringBuilder ids = new StringBuilder(people.length);
-//        String prefix = "";
-//        for (Person person : people) {
-//            ids.append(prefix);
-//            prefix = ",";
-//            ids.append(person.getId().toString());
-//        }
-
-        // with funional programming
-        String ids = Arrays.stream(people)
-                .map(Person::getId)
-                .map(String::valueOf)
-                .collect(joining(","));
-        try {
-            Statement stmt = super.connection.createStatement();
-            int affectedRecordCount = stmt.executeUpdate("DELETE FROM PEOPLE WHERE ID IN(:ids)".replace(":ids", ids));
-            System.out.println(affectedRecordCount);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public void update(Person person) {
-        try {
-            PreparedStatement ps = super.connection.prepareStatement("UPDATE PEOPLE SET FIRST_NAME=?, LAST_NAME=?, DOB=?, SALARY=? WHERE ID=?");
-            ps.setString(1, person.getFirstName());
-            ps.setString(2, person.getLastName());
-            ps.setTimestamp(3, this.convertDobToTimestamp(person.getDob()));
-            ps.setBigDecimal(4, person.getSalary());
-            ps.setLong(5, person.getId());
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+    @Override
+    protected String getUpdateSql() {
+        return UPDATE_SQL;
     }
 
     // Ctrl + Alt + P no se doi tu person.dob -> ZonedDateTime dob
